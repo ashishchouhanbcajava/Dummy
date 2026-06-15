0692b4bd3373052171ebb1533289b3f7ad5d1f16
Ashish <chouhanashish5678@gmail.com>
2026-06-15 17:17:08 +0530

    added basic auth config

--- src/main/java/com/Dummy/Dummy/configurations/SecurityConfig.java
+++ src/main/java/com/Dummy/Dummy/configurations/SecurityConfig.java
@@ -2,17 +2,21 @@ package com.Dummy.Dummy.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
 public class SecurityConfig {
 
-//	@Bean
-//	SecurityFilterChain config(HttpSecurity http) {
-//		
-//		return http.cors().disable().REQU
-//	}
+	// basic auth configuration for sending username and passsword with each and
+	// every request
+	@Bean
+	SecurityFilterChain config(HttpSecurity http) throws Exception {
+
+		return http.authorizeHttpRequests(au -> au.anyRequest().authenticated()).httpBasic(Customizer.withDefaults())
+				.build();
+	}
 }
